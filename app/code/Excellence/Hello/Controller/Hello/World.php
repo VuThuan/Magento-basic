a55e0d12420566fe4d7ec4e0d616aedb5e06bb0c
vuthuan <vuthuan3090@gmail.com>
2019-11-05 10:27:04 +0700

    session cookie

--- app/code/Excellence/Hello/Controller/Hello/World.php
+++ app/code/Excellence/Hello/Controller/Hello/World.php
@@ -7,21 +7,55 @@ class World extends \Magento\Framework\App\Action\Action
 
     protected $_registry;
 
+    protected $request;
+
+    protected $_catalogSession;
+    protected $_customerSession;
+    protected $_checkoutSession;
+
+    protected $_cookieManager;
+
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\Registry $registry
+        \Magento\Framework\App\Request\Http $request,
+        \Magento\Framework\Registry $registry,
+        \Magento\Catalog\Model\Session $catalogSession,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        array $data = [],
+        \Magento\Framework\Stdlib\CookieManagerInterface $cookieManager
         )
     {
+        $this->request = $request; 
         $this->_registry = $registry;
+
+        $this->_catalogSession = $catalogSession;
+        $this->_customerSession = $customerSession;
+        $this->_checkoutSession = $checkoutSession;
+
+        $this->_cookieManager = $cookieManager;
         return parent::__construct($context);
     }
 
+    //Hello world
     public function execute()
     {
         echo 'Hello World';
-        exit;        
+        // $this->getCatalogSession()->setMyname('Mageplaza');
+        // echo  $this->getCatalogSession()->getMyName(). '</br>';
+        // $cookieValue = $this->_cookieManager->getCookie(\Mageplaza\HelloWorld\Controller\Cookie\AddCookie::COOKIE_NAME);
+        // echo $cookieValue;
+        exit;
     }
 
+    //Request
+    public function methodName()
+    {
+        $this->request->getParams(); //get All request
+        $this->request->getParam('id-123'); //Get one request with key='id-123'
+    }
+
+    //Registry
     public function getProduct()
     {
         return $this->_registry->registry('current_product');
@@ -32,5 +66,24 @@ class World extends \Magento\Framework\App\Action\Action
         return $this->_registry->registry('current_category');
     }
 
-    
+    public function getCurrentCMSPage()
+    {
+        return $this->_registry->registry('current_cms_page');
+    }
+
+    //Session
+    public function getCatalogSession()
+    {
+        return $this->_catalogSession;
+    }
+
+    public function getCustomerSession()
+    {
+        return $this->_customerSession;
+    }
+
+    public function getCheckoutSession()
+    {
+        return $this->_checkoutSession;
+    }
 }
