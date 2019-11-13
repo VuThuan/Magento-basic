6ab291ed4c1e86260f116926794f9787c69d84cf
vuthuan <vuthuan3090@gmail.com>
2019-11-13 22:28:36 +0700

    add paractive part 3: plugin inject variable

---
+++ app/code/ParactivePart3/PluginInjectVariable/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'ParactivePart3_PluginInjectVariable',
+    __DIR__
+);
